928c9266bee3926db3896d6383733613299b16fa
Eunice000 <chiuchungyiu@gmail.com>
2022-06-01 09:02:39 +0800

    RSM module

---
+++ RSM/RSM/RestockRequestForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace RSM
+{
+    partial class RestockRequestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RRFLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RRFLbl
+            // 
+            this.RRFLbl.BackColor = System.Drawing.SystemColors.Info;
+            this.RRFLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RRFLbl.Location = new System.Drawing.Point(-2, -4);
+            this.RRFLbl.Name = "RRFLbl";
+            this.RRFLbl.Size = new System.Drawing.Size(803, 46);
+            this.RRFLbl.TabIndex = 1;
+            this.RRFLbl.Text = "Restock Request Form";
+            this.RRFLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // RestockRequestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RRFLbl);
+            this.Name = "RestockRequestForm";
+            this.Text = "RestockRequestForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label RRFLbl;
+    }
+}
---
+++ RSM/RSM/RestockRequestRecord.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace RSM
+{
+    partial class RestockRequestRecord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RRRLbl = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RRRLbl
+            // 
+            this.RRRLbl.BackColor = System.Drawing.SystemColors.Info;
+            this.RRRLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RRRLbl.Location = new System.Drawing.Point(-1, -1);
+            this.RRRLbl.Name = "RRRLbl";
+            this.RRRLbl.Size = new System.Drawing.Size(803, 46);
+            this.RRRLbl.TabIndex = 0;
+            this.RRRLbl.Text = "Restock Request Record";
+            this.RRRLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 174);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // RestockRequestRecord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.RRRLbl);
+            this.Name = "RestockRequestRecord";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.RestockRequestRecord_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label RRRLbl;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
